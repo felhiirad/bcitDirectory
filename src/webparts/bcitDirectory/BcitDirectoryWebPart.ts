476d05157baab76520606bff834bba7bf4da0c4d
Slim Ben Taieb <slim.bentaieb@outlook.com>
2021-10-06 14:47:18 +0100

    First commit

---
+++ src/webparts/bcitDirectory/BcitDirectoryWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'BcitDirectoryWebPartStrings';
+import BcitDirectory from './components/BcitDirectory';
+import { IBcitDirectoryProps } from './components/IBcitDirectoryProps';
+
+export interface IBcitDirectoryWebPartProps {
+  description: string;
+}
+
+export default class BcitDirectoryWebPart extends BaseClientSideWebPart<IBcitDirectoryWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IBcitDirectoryProps> = React.createElement(
+      BcitDirectory,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/bcitDirectory/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBcitDirectoryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BcitDirectoryWebPartStrings' {
+  const strings: IBcitDirectoryWebPartStrings;
+  export = strings;
+}
